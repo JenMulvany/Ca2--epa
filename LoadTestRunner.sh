d9a5384581e844742dccbe85b0bad226da847216
epa <epa@localhost.localdomain>
2017-12-16 13:35:41 +0000

    commit final

--- LoadTestRunner.sh
+++ LoadTestRunner.sh
@@ -5,7 +5,7 @@
 
 count="8"
 #creating the columns in the results doc
-echo -e "CO\t N\t idle" > results.dat
+echo -e "CO\t N\t busy" > results.dat
 
 #forloop to run the load test in the rang 1 <= 50 starting at 1 and increasing each time
 for ((i=1; i <= 50; i++))
@@ -13,14 +13,16 @@ do
  ./loadtest $i &
  
 #collecting cpu utilization
- idle=$(mpstat $count 1 -o JSON  | jq '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') 
+ busy=$(mpstat $count 1 -o JSON  | jq '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') 
 
 
 
  CO=$(cat synthetic.dat | wc -l)
 
 #outputing the results the the results.dat file
- echo -e "$CO\t $i\t $idle\t" >>   results.dat
+ echo -e "$CO\t $i\t $busy\t" >>   results.dat
+
+#kills the process ./loadtest
 
 pkill loadtest
 
